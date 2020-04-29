c436a9242da5df3d6e44b6e7b33d00bfea2a71c0
David Orson <onlyorson@outlook.com>
2020-04-29 19:48:54 +0100

    Exercise 3 of Eloquent JS

--- eloquent-javascript/2-program-structure.js
+++ eloquent-javascript/2-program-structure.js
@@ -36,3 +36,19 @@ function fizzOrBuzz() {
 }
 
 fizzOrBuzz();
+
+/* When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those). */
+
+function fizzBuzz() {
+  for (i = 1; i < 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else console.log(i);
+  }
+}
+
+fizzBuzz();
