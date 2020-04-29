e3c521caaf2809d674fb98834a4fa8afd12601d7
David Orson <onlyorson@outlook.com>
2020-04-29 20:17:18 +0100

    Exercise 4 of Eloquent JS

--- eloquent-javascript/2-program-structure.js
+++ eloquent-javascript/2-program-structure.js
@@ -52,3 +52,60 @@ function fizzBuzz() {
 }
 
 fizzBuzz();
+
+// Chessboard
+
+/* Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a "#" character. The characters should form a chessboard. */
+
+function chessboard() {
+  let chessboardString = " # # # #\n";
+  for (let i = 0; i < 8; i++) {
+    if (i === 0) {
+    } else {
+      if (i % 2 === 0) {
+        chessboardString += " # # # #\n";
+      } else {
+        chessboardString += "# # # # \n";
+      }
+    }
+  }
+  console.log(chessboardString);
+}
+
+chessboard();
+
+/* When you have a program that generates this pattern, define a binding size = 8 and change the program so that it works for any size, outputting a grid of the given width and height. */
+
+function makeCheckerboard(size) {
+  let evenString = "";
+  for (i = 0; i < size; i++) {
+    if (i % 2 === 0) {
+      evenString = `${evenString} `;
+    } else {
+      evenString = `${evenString}#`;
+    }
+  }
+  evenString += "\n";
+
+  let oddString = "";
+  for (i = 0; i < size; i++) {
+    if (i % 2 === 0) {
+      oddString = `${oddString}#`;
+    } else {
+      oddString = `${oddString} `;
+    }
+  }
+  oddString += "\n";
+
+  let checkerboardString = "";
+  for (let i = 1; i <= size; i++) {
+    if (i % 2 === 0) {
+      checkerboardString += evenString;
+    } else {
+      checkerboardString += oddString;
+    }
+  }
+  console.log(checkerboardString);
+}
+
+makeCheckerboard(8);
